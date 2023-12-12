478e6a3da40aadb7a6abe3ec8e3678f3af71e384
rockey223 <rockeym50@gmail.com>
2023-12-12 16:04:30 +0545

    minor changes

--- src/Navbar/anim.js
+++ src/Navbar/anim.js
@@ -6,7 +6,7 @@ export const menuSlide = {
 
 export const slide = {
     initial: {x: 80},
-    enter: i => ({x: 0, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}}),
+    enter: i => ({x: 0, transition: {duration: 0.1, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}}),
     exit: i => ({x: 80, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}})
 }
 
--- src/Navbar/nav/Link/NavLink.jsx
+++ src/Navbar/nav/Link/NavLink.jsx
@@ -4,14 +4,18 @@ import './link.scss';
 import { Link } from 'react-scroll';
 import { motion } from 'framer-motion';
 import { slide, scale } from '../../anim';
+import { BiRightArrowAlt } from "react-icons/bi";
 
 export default function NavLink({data, isActive, setSelectedIndicator}) {
   
     const { title, path, index} = data;
   
     return (
-      <motion.div className='link' onMouseEnter={() => {setSelectedIndicator(path)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
-        <motion.div variants={scale} animate={isActive ? "open" : "closed"} className='indicator'></motion.div>
+      <motion.div className='link' onMouseEnter={() => {setSelectedIndicator(path)}} custom={index} variants={slide} initial="initial" whileHover={{
+        x: 20,
+       
+      }} animate="enter" exit="exit">
+        {/* <motion.div variants={scale} animate={isActive ? "open" : "closed"} className='indicator'></motion.div> */}
         {/* <NavLink to={path}>{title}</NavLink> */}
         <Link 
       // activeClass="active" 
